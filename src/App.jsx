ae5a4b56e0200f3d472c47b4aafc8e9b3a94eeab
petrushya <108490184+petrushya@users.noreply.github.com>
2026-02-07 11:32:56 +0300

    added styles to index.css App.css prettier App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState, Fragment } from 'react'
-import Nickname from './assets/nickname'
-import Person from './assets/person';
-import './App.css'
+import { useState, Fragment } from "react";
+import Nickname from "./assets/nickname";
+import Person from "./assets/person";
+import "./App.css";
 
 export default function App() {
-
   const [data, setData] = useState({});
 
   return (
     <>
-      {!data.nickname
-      ? <Nickname data={data} setData={setData} />
-      : <Fragment>
-        <h2>Welcome &ldquo;{data.nickname}&rdquo;</h2>
-        <p>Please fill out the following form.</p>
-        <Person data={data} setData={setData} />
-      </Fragment>
-      }
+      {!data.nickname ? (
+        <Nickname data={data} setData={setData} />
+      ) : (
+        <Fragment>
+          <h2>Welcome &ldquo;{data.nickname}&rdquo;</h2>
+          <p>Please fill out the following form.</p>
+          <Person data={data} setData={setData} />
+        </Fragment>
+      )}
     </>
-  )
+  );
 }
