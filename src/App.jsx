65b94b655d22b9d0395830f01e982d0a3c527702
petrushya <108490184+petrushya@users.noreply.github.com>
2026-02-08 16:46:15 +0300

    added Page & button for show page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,48 @@ import Nickname from "./assets/nickname";
 import Person from "./assets/person";
 import Education from "./assets/education";
 import Experience from "./assets/experience";
+import Page from "./assets/page";
 import "./App.css";
-
 
 export default function App() {
   const [data, setData] = useState({});
-{data.nickname && console.log(data)}
+  const [isEdit, setIsEdit] = useState(true);
+
+  const lockIsEditBtn = (function () {
+    return Object.keys(data).length === 4 ? false : true;
+  })();
+
+  function handleIsEdit() {
+    setIsEdit(!isEdit);
+  }
   return (
     <>
       {!data.nickname ? (
         <Nickname data={data} setData={setData} />
-      ) : (
+      ) : isEdit ? (
         <Fragment>
           <h2>Welcome &ldquo;{data.nickname}&rdquo;</h2>
           <p>Please fill out the following form.</p>
+          <hr />
           <Person data={data} setData={setData} />
           <Education data={data} setData={setData} />
           <Experience data={data} setData={setData} />
         </Fragment>
+      ) : (
+        <Page data={data} />
+      )}
+      <hr />
+      {data.nickname && (
+        <button
+          key="isEdit"
+          id="isEdit"
+          type="button"
+          onClick={handleIsEdit}
+          disabled={lockIsEditBtn}
+          className="top-margin"
+        >
+          {isEdit ? "save&show" : "redact"}
+        </button>
       )}
     </>
   );
