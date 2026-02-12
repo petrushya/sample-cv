ae2554fe815457030468d86419c9d7070994f78e
petrushya <108490184+petrushya@users.noreply.github.com>
2026-02-12 21:32:30 +0300

    changed sample.jsx

--- src/components/sample.jsx
+++ src/components/sample.jsx
@@ -2,33 +2,33 @@ export default function Page({ data }) {
 
   return (
     <section className="max-width-80">
-      <h2>Sample page for &ldquo;{data.nickname}&rdquo;</h2>
+      <h2>Sample page</h2>
       <hr />
       <dl>
-        <h2>{data.person.firstName + ' ' +
-          data.person.lastName}</h2>
+        <h2>{data.name + ' ' +
+          data.surname}</h2>
         <dt><h3>Education</h3></dt>
         <dd>
-          <b>Scool name:</b> &ldquo;{data.education.scool}&rdquo;<br />
-          <b>Title of study:</b> {data.education.title}<br />
-          <b>Programming skills:</b> {data.education.scills}<br />
-          <b>Date of study:</b> {'from ' + data.education.startEduc + ' to '
-          + (data.education.endEduc !== ''
-          ? data.education.endEduc : 'the present')}
+          <b>Scool name:</b> &ldquo;{data.scool}&rdquo;<br />
+          <b>Title of study:</b> {data.title}<br />
+          <b>Programming skills:</b> {data.scills}<br />
+          <b>Date of study:</b> {'from ' + data.startStudy + ' to '
+          + (data.endStudy !== ''
+          ? data.endStudy : 'the present')}
         </dd>
         <dt><h3>Practical experience</h3></dt>
         <dd>
-          <b>Company name:</b> &ldquo;{data.experience.company}&rdquo;<br />
-          <b>Position title:</b> {data.experience.position}<br />
-          <b>Main responsibilities:</b> {data.experience.duty}<br />
-          <b>Dates of work:</b> {'from ' + data.experience.startWork + ' to '
-          + (data.experience.endWork !== ''
-            ? data.experience.endWork : 'the present')}
+          <b>Company name:</b> &ldquo;{data.company}&rdquo;<br />
+          <b>Position title:</b> {data.position}<br />
+          <b>Main responsibilities:</b> {data.duties}<br />
+          <b>Dates of work:</b> {'from ' + data.startWork + ' to '
+          + (data.endWork !== ''
+            ? data.endWork : 'the present')}
         </dd>
         <dt><h3>Contacts</h3></dt>
         <dd>
-          <b>Phone:</b> {data.person.tel}<br />
-          <b>Email:</b> {data.person.email}
+          <b>Phone:</b> {data.tel}<br />
+          <b>Email:</b> {data.email}
         </dd>
       </dl>
     </section>
