a230cc3bfc3736a53b9bc4838c62aaec07fbdf0c
petrushya <108490184+petrushya@users.noreply.github.com>
2026-02-18 17:34:37 +0300

    edited person.jsx

--- src/components/person.jsx
+++ src/components/person.jsx
@@ -25,84 +25,82 @@ export default function Person({
 
   return (
     <>
-    <div className="e-fl e-c j-b bg-gray">
-      <h2>General information</h2>
-      <button onClick={()=>setVisualP(!visualP)}>
-        {visualP ? <span>&#x25B2;</span> : <span>&#x25BC;</span>}
-      </button>
-    </div>
+      <div className="e-fl e-c j-b bg-gray">
+        <h2>General information</h2>
+        <button name="show/hide" onClick={() => setVisualP(!visualP)}>
+          {visualP ? <span>&#x25B2;</span> : <span>&#x25BC;</span>}
+        </button>
+      </div>
       {visualP && (
-      <form
-        className="e-fl d-cl t-m"
-        onSubmit={handlePerson}
-      >
-        <h2 className="sf-s">Person details</h2>
-        <label>
-          Name:
-          <input
-            id="name"
-            name="first name"
-            placeholder="Name"
-            autoComplete="off"
-            value={person.name}
-            onChange={(e) => setPerson({ ...person, name: e.target.value })}
-            required
-          />
-          <span></span>
-        </label>
-        <label>
-          Surname:
-          <input
-            id="surname"
-            name="surname"
-            placeholder="Surname"
-            autoComplete="off"
-            value={person.surname}
-            onChange={(e) => setPerson({ ...person, surname: e.target.value })}
-            required
-          />
-          <span></span>
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="wood@good.win"
-            autoComplete="off"
-            value={person.email}
-            onChange={(e) => setPerson({ ...person, email: e.target.value })}
-            required
-          />
-          <span></span>
-        </label>
-        <label className="e-fl">
-          Phone number:
-          <div>
-            <small className="e-bl">Format: 123-456-7890</small>
+        <form className="e-fl d-cl t-m" onSubmit={handlePerson}>
+          <h2 className="sf-s">Person details</h2>
+          <label>
+            Name:
             <input
-              type="tel"
-              id="phone"
-              name="phone number"
-              placeholder="123-456-7890"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              id="name"
+              name="first name"
+              placeholder="Name"
               autoComplete="off"
-              value={person.phone}
-              onChange={(e) => setPerson({ ...person, phone: e.target.value })}
+              value={person.name}
+              onChange={(e) => setPerson({ ...person, name: e.target.value })}
               required
             />
             <span></span>
-          </div>
-        </label>
-        <button
-          id="btnPerson"
-          className="sf-c t-m"
-          disabled={lockButton}
-        >
-          confirm
-        </button>
-      </form>)}
+          </label>
+          <label>
+            Surname:
+            <input
+              id="surname"
+              name="surname"
+              placeholder="Surname"
+              autoComplete="off"
+              value={person.surname}
+              onChange={(e) =>
+                setPerson({ ...person, surname: e.target.value })
+              }
+              required
+            />
+            <span></span>
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="wood@good.win"
+              autoComplete="off"
+              value={person.email}
+              onChange={(e) => setPerson({ ...person, email: e.target.value })}
+              required
+            />
+            <span></span>
+          </label>
+          <label className="e-fl">
+            Phone number:
+            <div>
+              <small className="e-bl">Format: 123-456-7890</small>
+              <input
+                type="tel"
+                id="phone"
+                name="phone number"
+                placeholder="123-456-7890"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                autoComplete="off"
+                value={person.phone}
+                onChange={(e) =>
+                  setPerson({ ...person, phone: e.target.value })
+                }
+                required
+              />
+              <span></span>
+            </div>
+          </label>
+          <button name="confirm" className="sf-c t-m" disabled={lockButton}>
+            confirm
+          </button>
+        </form>
+      )}
     </>
   );
 }
