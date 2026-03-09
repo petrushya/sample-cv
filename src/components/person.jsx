91738b82bca1cbefba6083c8c35af3a671c4e779
petrushya <108490184+petrushya@users.noreply.github.com>
2026-03-09 10:43:55 +0300

    changed person.jsx

--- src/components/person.jsx
+++ src/components/person.jsx
@@ -1,112 +1,121 @@
-export default function Person({
-  person,
-  setPerson,
-  data,
-  setData,
-  visualP,
-  setVisualP,
-}) {
-  function handlePerson(e) {
+import { useState } from "react";
+
+export default function Person({ data, setData, preview, visualP, setVisualP }) {
+  const helpData = {};
+  Object.keys(data)
+    .slice(0, 4)
+    .map((key) => (helpData[key] = data[key]));
+  const [person, setPerson] = useState(helpData);
+
+  function handleSavePerson(e) {
     e.preventDefault();
-    setData(person);
+    Object.keys(data)
+      .slice(0, 4)
+      .map((key) => (helpData[key] = data[key]));
+    setPerson(helpData);
   }
 
   function lockButton() {
     const dataArr = Object.values(data).slice(0, 4);
-    const personArr = Object.values(person).slice(0, 4);
-    if (personArr.filter((item) => !dataArr.includes(item)).length) {
+    const personArr = Object.values(person);
+    if (dataArr.filter((item) => !personArr.includes(item)).length) {
       return false;
     } else {
       return true;
     }
   }
 
+  function handleFaceForm() {
+    setVisualP(!visualP);
+  }
+
   return (
-    <section className="all-bdr">
-      <div className="e-flx jst-bw pdn-ln bg-gray">
-        <h2 className="pdn-bl">General information</h2>
-        <button
-          className="bg-trs"
-          name="show/hide"
-          onClick={() => setVisualP(!visualP)}
-        >
-          {visualP ? <span>&#x25B2;</span> : <span>&#x25BC;</span>}
-        </button>
-      </div>
-      {visualP && (
-        <form className="e-flx aln-d dir-cl" onSubmit={handlePerson}>
-          <label>
-            Name:
-            <input
-              id="name"
-              name="name"
-              placeholder="Name"
-              autoComplete="off"
-              value={person.name}
-              onChange={(e) => setPerson({ ...person, name: e.target.value })}
-              autoFocus
-              required
-            />
-            <span></span>
-          </label>
-          <label>
-            Surname:
-            <input
-              id="surname"
-              name="surname"
-              placeholder="Surname"
-              autoComplete="off"
-              value={person.surname}
-              onChange={(e) =>
-                setPerson({ ...person, surname: e.target.value })
-              }
-              required
-            />
-            <span></span>
-          </label>
-          <label>
-            Email:
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="wood@good.win"
-              autoComplete="off"
-              value={person.email}
-              onChange={(e) => setPerson({ ...person, email: e.target.value })}
-              required
-            />
-            <span></span>
-          </label>
-          <label className="e-flx aln-d">
-            Phone number:
-            <div>
-              <small className="blk">Format: 123-456-7890</small>
-              <input
-                type="tel"
-                id="phone"
-                name="phone"
-                placeholder="123-456-7890"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                autoComplete="off"
-                value={person.phone}
-                onChange={(e) =>
-                  setPerson({ ...person, phone: e.target.value })
-                }
-                required
-              />
-              <span></span>
-            </div>
-          </label>
-          <button
-            name="confirm"
-            className="slf-c mrn-t"
-            disabled={lockButton()}
-          >
-            confirm
-          </button>
-        </form>
+    <>
+      {preview ? (
+        <section key="global" className="all-bdr">
+          <div className="e-flx jst-bw pdn-ln bg-gray">
+            <h2 className="pdn-bl">General information</h2>
+            <button className="bg-trs" name="visibilityP" onClick={handleFaceForm}>
+              {visualP ? <span>&#x25B2;</span> : <span>&#x25BC;</span>}
+            </button>
+          </div>
+          {visualP && (
+            <form onSubmit={handleSavePerson} className="e-flx dir-cl">
+              {Object.keys(data)
+                .slice(0, 4)
+                .map((key) =>
+                  key !== "phone" ? (
+                    <div key={key + "Person"} className="e-flx flx-wrp mrn-bt dw-100">
+                      <label htmlFor={key} className="flx-b">
+                        {key === "name" ? "Name:" : key === "surname" ? "Surname:" : "Email:"}
+                      </label>
+                      <div className="flx-b e-flx aln-t">
+                        <input
+                          id={key}
+                          name={key}
+                          type={key === "email" ? "email" : "text"}
+                          placeholder={key === "name" ? "Name" : key === "surname" ? "Surname"
+                          : "wood@good.win"}
+                          autoComplete="off"
+                          value={data[key]}
+                          onChange={(e) => setData({ ...data, [key]: e.target.value })}
+                          autoFocus={key === "name" ? true : false}
+                          required
+                        />
+                        <span></span>
+                      </div>
+                    </div>
+                  ) : (
+                    <div key={key + "Person"} className="e-flx flx-wrp mrn-bt">
+                      <label htmlFor={key} className="flx-b">
+                        Phone number (<small>Format: 123-456-7890</small>):
+                      </label>
+                      <div className="flx-b e-flx aln-t">
+                        <input
+                          id={key}
+                          name={key}
+                          type="tel"
+                          placeholder="123-456-7890"
+                          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                          autoComplete="off"
+                          value={data.phone}
+                          onChange={(e) => setData({ ...data, phone: e.target.value })}
+                          required
+                        />
+                        <span></span>
+                      </div>
+                    </div>
+                  ),
+                )}
+              <button name="confirm" className="mrn-t" disabled={lockButton()}>
+                confirm
+              </button>
+            </form>
+          )}
+        </section>
+      ) : (
+        <section key="globalSample" className="e-flx aln-d jst-bw pdn-bl pdn-ln bdr-bt">
+          <h2 className="pdn-bl mrn-bt txt-lt">
+            <em>{data.name + " " + data.surname}</em>
+          </h2>
+          <table>
+            <tbody>
+              <tr>
+                <th>Email:</th>
+                <td>
+                  <em>{data.email}</em>
+                </td>
+              </tr>
+              <tr>
+                <th>Phone:</th>
+                <td>
+                  <em>{data.phone}</em>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
       )}
-    </section>
+    </>
   );
 }
